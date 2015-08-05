ee22d16e49dbd09d4ebe93a4ce2c33cadc0b077c
Alvaro Vilanova Vidal <alvivi@gmail.com>
2015-08-05 15:35:47 +0200

    Added actual code

---
+++ xcode6-project/tableview2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableview2
+//
+//  Created by Alvaro Vilanova Vidal on 5/8/15.
+//  Copyright (c) 2015 hacking.bar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ xcode6-project/tableview2Tests/tableview2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  tableview2Tests.swift
+//  tableview2Tests
+//
+//  Created by Alvaro Vilanova Vidal on 5/8/15.
+//  Copyright (c) 2015 hacking.bar. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class tableview2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
